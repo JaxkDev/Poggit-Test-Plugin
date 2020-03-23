6683fc4146d1a9ab7fd1b1226daeed1c5b964969
Jack Honour <JaxkDev@gmail.com>
2020-03-23 19:34:05 +0000

    Update Main.php

--- src/JaxkDev/TestPlugin/Main.php
+++ src/JaxkDev/TestPlugin/Main.php
@@ -6,13 +6,19 @@ namespace JaxkDev\TestPlugin;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase{
+	
+	/** @var int */
+	$db = 0;
+	
 	public function onEnable(){
+		$this->db = 1;
 		$this->getLogger()->info("Enabled.", 2);
 		echo "hi there.";
 	}
 	
 	public function onDisable(){
 		$this->getLogger()->info("Disabled.");
+		echo $this->db["hi"];
 	}
 }
 
